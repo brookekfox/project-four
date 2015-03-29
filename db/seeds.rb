78ea8d0779d88eff69d8bf1d79cedaa931ca703b
brookekfox <brookekfox@gmail.com>
2015-03-28 19:00:23 -0700

    added precalc quiz seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Precalc.create(
 							 'This relationship is a function when each input can have exactly one output.',
 							 'The set of codomain objects that are actually used by the function is called the range of the
 function.'
-							],
+		],
 		module_name: 'functions'
 )
 
@@ -36,7 +36,7 @@ Precalc.create(
 							 '$x$-intercept of $f(x)$: DNE, $x$-intercept of $f^{-1}(x)$: $1$',
 							 '$y$-intercept of $f(x)$: $1$, $y$-intercept of $f^{-1}(x)$: DNE',
 							 'Asymptote of $f(x)$: $y = 0$, asymptote of $f^{-1}(x)$: $x = 0$'
-							],
+		],
 		module_name: 'logs',
 		hint:'Use logarithms to find the inverse function. Draw $f(x)$ and $f^{-1}(x)$ on the same plane. Also note that $$\\text{dom}(f) = \\text{ran}(f^{-1}).$$'
 )
@@ -50,7 +50,7 @@ Precalc.create(
 							 '$x$-intercept of $f(x)$: DNE, $x$-intercept of $f^{-1}(x)$: $1$',
 							 '$y$-intercept of $f(x)$: $1$, $y$-intercept of $f^{-1}(x)$: DNE',
 							 'Asymptote of $f(x)$: $y = 0$, asymptote of $f^{-1}(x)$: $x = 0$'
-							],
+		],
 		module_name: 'logs',
 		hint:'Use logarithms to find the inverse function. Draw $f(x)$ and $f^{-1}(x)$ on the same plane. Also note that $$\\text{dom}(f) = \\text{ran}(f^{-1}).$$'
 )
@@ -66,7 +66,7 @@ $$\\begin{align} \\ln{(e^x)} &= \\ln{4} \\newline x\\ln{e} &= \\ln{4} \\newline 
 							 'If $e^x+2 = 0$, then $e^x = -2$. Taking the natural logarithm of both sides, we get
 $$\\begin{align} \\ln{(e^x)} &= \\ln{(-2)} \\newline x\\ln{e} &= \\ln{(-2)} \\newline x &= \\ln{(-2)}, \\end{align}$$ which is undefined.',
 							 'Thus, $x = \\ln{4} \\approx 1.3863$.'
-							],
+		],
 		module_name: 'logs',
 		hint: 'Make a polynomial of order 2 and factor.'
 )
@@ -87,42 +87,42 @@ Precalc.create(
 )
 
 Precalc.create(
-		question:'Describe the maximum and minimum of \$\\sin{x}\$.',
+		question:'Describe the maximum and minimum of $\\sin{x}$.',
 		question_type: 'practice',
-		solution: ['The maximum value of \$x\$ is \$1\$.',
-							 'The maximum value of \$x\$ occurs at $$\\frac{\\pi}{2} + 2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-\\frac{3\\pi}{2},\\frac{\\pi}{2},\\frac{5\\pi}{2},\\cdots \\right\\}.$$',
-							 'The minimum value of \$\\sin{x}\$ is \$-1\$.',
-							 'The minimum value of \$\\sin{x}\$ occurs at $$-\\frac{\\pi}{2} + 2\\pi n \\text{ and } \\frac{3\\pi}{2} + 2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-\\frac{5\\pi}{2},-\\frac{\\pi}{2},\\frac{3\\pi}{2},\\frac{7\\pi}{2},
+		solution: ['The maximum value of $x$ is $1$.',
+							 'The maximum value of $x$ occurs at $$\\frac{\\pi}{2} + 2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-\\frac{3\\pi}{2},\\frac{\\pi}{2},\\frac{5\\pi}{2},\\cdots \\right\\}.$$',
+							 'The minimum value of $\\sin{x}$ is $-1$.',
+							 'The minimum value of $\\sin{x}$ occurs at $$-\\frac{\\pi}{2} + 2\\pi n \\text{ and } \\frac{3\\pi}{2} + 2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-\\frac{5\\pi}{2},-\\frac{\\pi}{2},\\frac{3\\pi}{2},\\frac{7\\pi}{2},
 \\cdots \\right\\}.$$'
-							],
+		],
 		module_name: 'sine',
 		hint: 'Draw the graph of sine or reference the unit circle.'
 )
 
 Precalc.create(
-		question:'Describe the maximum and minimum of \$\\cos{x}\$.',
+		question:'Describe the maximum and minimum of $\\cos{x}$.',
 		question_type: 'practice',
-		solution: ['The maximum value of \$x\$ is \$1\$.',
-							 'The maximum value of \$x\$ occurs at $$2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-2\\pi n, 0, 2\\pi n, 4\\pi n,\\cdots \\right\\}.$$',
-							 'The minimum value of \$\\cos{x}\$ is \$-1\$.',
-							 'The minimum value of \$\\cos{x}\$ occurs at $$-\\pi + 2\\pi n \\text{ and } \\pi + 2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-\\pi,\\pi,3\\pi,\\cdots \\right\\}.$$'
+		solution: ['The maximum value of $x$ is $1$.',
+							 'The maximum value of $x$ occurs at $$2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-2\\pi n, 0, 2\\pi n, 4\\pi n,\\cdots \\right\\}.$$',
+							 'The minimum value of $\\cos{x}$ is $-1$.',
+							 'The minimum value of $\\cos{x}$ occurs at $$-\\pi + 2\\pi n \\text{ and } \\pi + 2\\pi n, n \\in \\mathbb{Z} \\text{ or } \\left\\{ \\cdots,-\\pi,\\pi,3\\pi,\\cdots \\right\\}.$$'
 		],
 		module_name: 'sine',
 		hint: 'Draw the graph of cosine or reference the unit circle.'
 )
 
 Precalc.create(
-		question:'Identify the amplitude, period, vertical shift, and phase shift of \$y = 3\\sin{\\left(
-2x-\\frac{\\pi}{2}\\right)}\$',
+		question:'Identify the amplitude, period, vertical shift, and phase shift of $y = 3\\sin{\\left(
+2x-\\frac{\\pi}{2}\\right)}$',
 		question_type: 'practice',
 		solution: [
-								'The amplitude is \$3\$.',
-								'The period is \$\\pi\$, and the domain is \$(-\\infty,\\infty)\$.',
-								'The vertical shift is \$0\$.',
-								'The phase shift is \$-\\frac{\\pi}{4}\$.'
-							],
+				'The amplitude is $3$.',
+				'The period is $\\pi$, and the domain is $(-\\infty,\\infty)$.',
+				'The vertical shift is $0$.',
+				'The phase shift is $-\\frac{\\pi}{4}$.'
+		],
 		module_name: 'sine',
-		hint: 'Write \$y\$ in the form $$f(x) = A \\sin{(Bx - C)} + D.$$'
+		hint: 'Write $y$ in the form $$f(x) = A \\sin{(Bx - C)} + D.$$'
 )
 
 Precalc.create(
@@ -133,7 +133,8 @@ Precalc.create(
 							 'First, we solve the second equation for $y$: $$\\begin{align} 4x+y &= 24 \\newline y &= 24 - 4x
 \\end{align}$$',
 							 'Now plug into the first equation the value we found for $y$: $$2x - 3\\left(24-4x\\right) = -2$$',
-							 'Simplify: $$\\begin{align} 2x - 72 + 12x &= -2 \\newline 2x + 12x &= 70 \\newline 14x &= 70 \\newline x &= 5 \\end{align}$$',
+							 'Simplify: $$\\begin{align} 2x - 72 + 12x &= -2 \\newline 2x + 12x &= 70 \\newline 14x &= 70 \\newline x &= 5
+\\end{align}$$',
 							 'Now, plug in $x=5$ to either equation. We will use the first equation: $$\\begin{align} 2x-3y &= -2 \\newline 2(5)-3y &= -2 \\newline 10-3y &= -2 \\newline -3y &= -12 \\newline y &= 4 \\end{align}$$',
 							 'We check the answers.',
 							 'Using the second equation, $$\\begin{align}4(5)+(4) &= 24 \\newline 20+4 &= 24 \\newline 24 &= 24 \\end{align}$$'],
@@ -142,13 +143,13 @@ Precalc.create(
 )
 
 Precalc.create(
-		question: 'Solve the linear system of equations $$\\begin{align} 2x+y \&= 9 \\newline 3x-y \&=16 \\end{align}$$
+		question: 'Solve the linear system of equations $$\\begin{align} 2x+y &= 9 \\newline 3x-y &=16 \\end{align}$$
 by any method.',
 		question_type: 'practice',
 		solution: ['We solve by elimination.',
 							 'Add the two equations together to cancel the $y$: $$\\begin{align} 2x+y &= 9 \\newline 3x-y &=16 \\newline \\hline 5x &= 25 \\end{align}$$',
 							 'Solve for $x$: $$\\begin{align} 5x &= 25 \\newline x &= 5 \\end{align}$$',
-							 'Now use the value for $x$ to solve for the value of $y$: $$\\begin{align} 2(5)+y &= 9 \\newline 10+y &= 9 \\newline y &= -1 \\end{align}$$',
+							 'Now use the value for $x$ to solve for the value of $y$: $$\\begin{align} 2(5) + y &= 9 \\newline 10 + y &= 9 \\newline y &= -1 \\end{align}$$',
 							 'Now check the solution by plugging in the values: $$\\begin{align} 3(5)-(-1) &= 16 \\newline 15 + 1
 &= 16 \\newline 16 &= 16 \\end{align}$$'],
 		module_name: 'systems',
@@ -162,7 +163,7 @@ Precalc.create(
 							 'First, rewrite cotangent and cosecant in terms of sine and cosine: $$\\frac{\\cot{x}}{\\csc{x}} = \\frac{\\frac{\\cos{x}}{\\sin{x}}}{\\frac{1}{\\sin{x}}}$$',
 							 'Then, divide and simplify: $$\\frac{\\frac{\\cos{x}}{\\sin{x}}}{\\frac{1}{\\sin{x}}} = \\frac{\\cos{x}}{\\sin{x}} \\cdot \\frac{\\sin{x}}{1} = \\frac{\\cos{x}}{1} = \\cos{x}$$',
 							 'Now we confirm that $$\\cos{x} = \\frac{\\cot{x}}{\\csc{x}} = \\cos{x}.$$'
-							],
+		],
 		module_name: 'trig',
 		hint: 'Work with ONE side of the equation at a time. Start with the LHS.'
 )
@@ -172,25 +173,25 @@ Precalc.create(
 		question_type: 'practice',
 		solution: ['First, rewrite $\\tan{\\theta}$ and $\\cot{\\theta}$ as $$\\tan{\\theta}+\\cot{\\theta}
     = \\frac{\\sin{\\theta}}{\\cos{\\theta}} + \\frac{\\cos{\\theta}}{\\sin{\\theta}}.$$',
-			'Then, finding a common denominator, we have
+							 'Then, finding a common denominator, we have
 				$$\\frac{\\sin{\\theta}}{\\cos{\\theta}} + \\frac{\\cos{\\theta}}{\\sin{\\theta}} = \\frac{\\sin^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} + \\frac{\\cos^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}}.$$',
-		'Simplifying the numerators, we get $$\\frac{\\sin^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} + \\frac{\\cos^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} = \\frac{\\sin^2{\\theta} +
+							 'Simplifying the numerators, we get $$\\frac{\\sin^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} + \\frac{\\cos^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} = \\frac{\\sin^2{\\theta} +
 \\cos^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} = \\frac{1}{\\cos{\\theta}\\sin{\\theta}},$$ by the Pythagorean
 Identity.'],
 		module_name: 'trig',
 		hint:'Use the Pythagorean Identity: $$\\sin^2{\\theta} + \\cos^2{\\theta} = 1$$'
 )
 
+
+# ----------------------------------------      QUIZ QUESTIONS -------------------------------------------
 Precalc.create(
 		question: 'Write $\\tan{\\theta} + \\cot{\\theta}$ in terms of sine and cosine, and then simplify the expression.',
 		question_type: 'quiz',
 		module_name: 'trig',
-		answer_choices: [
-				'$\\cos{\\theta}\\sin{\\theta}$',
-				'$\\dfrac{1}{\\cos{\\theta}\\sin{\\theta}}$',
-				'$\\dfrac{\\sin^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} +
-\\dfrac{\\cos^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}}$'
-										],
+		answer_choices: ['$\\cos{\\theta}\\sin{\\theta}$',
+										 '$\\dfrac{1}{\\cos{\\theta}\\sin{\\theta}}$',
+										 '$\\dfrac{\\sin^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}} +
+\\dfrac{\\cos^2{\\theta}}{\\cos{\\theta}\\sin{\\theta}}$'],
 		correct: '$\\dfrac{1}{\\cos{\\theta}\\sin{\\theta}}$'
 )
 
@@ -198,56 +199,53 @@ Precalc.create(
 		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
 		question_type: 'quiz',
 		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
-		correct: '$-\\frac{\\pi}{4}$',
-		module_name: 'sine'
-)
-
-Precalc.create(
-		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
-		question_type: 'quiz',
-		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
+										 '$\\frac{\\pi}{2}$',
+										 '$-\\pi$'],
 		correct: '$-\\frac{\\pi}{4}$',
 		module_name: 'sine'
 )
 Precalc.create(
-		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
+		question: 'Solve $5(2^x) - 3 = 14$',
 		question_type: 'quiz',
-		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
-		correct: '$-\\frac{\\pi}{4}$',
-		module_name: 'sine'
+		answer_choices: ['$x = \\ln{2}$','$x \\approx 1.7655$','$x \\approx 1.2238$'],
+		correct: '$x \\approx 1.7655$',
+		module_name: 'logs'
 )
 Precalc.create(
-		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
+		question:'Given that $\\cos{x} = -\\frac{3}{5}$ for $\\pi < x < \\frac{3\\pi}{2}$, find $\\sin{2x}$.',
 		question_type: 'quiz',
-		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
-		correct: '$-\\frac{\\pi}{4}$',
-		module_name: 'sine'
+		answer_choices: ['$\\frac{12}{25}$','$-\\frac{4}{5}$','$\\frac{24}{25}$'],
+		correct: '$\\frac{24}{25}$',
+		module_name: 'trig'
 )
 Precalc.create(
-		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
+		question: 'Solve the following system of equations: $$\\begin{align} x+y &= 2 \\newline 2x-3y &= 9.
+\\end{align}$$',
 		question_type: 'quiz',
-		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
-		correct: '$-\\frac{\\pi}{4}$',
-		module_name: 'sine'
+		answer_choices: ['$(-3,1)$','$(2,1)$','$(-3,2)$'],
+		correct: '$(-3,1)$',
+		module_name: 'systems'
 )
 Precalc.create(
-		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
+		question:'What is the interest rate necessary for an investment to triple after 12 years of continuous compound
+interest?',
 		question_type: 'quiz',
-		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
-		correct: '$-\\frac{\\pi}{4}$',
-		module_name: 'sine'
+		answer_choices: ['$11.6\%$','$9.2\%$','$0.92\%$'],
+		correct: '$9.2\%$',
+		module_name: 'logs'
 )
 Precalc.create(
-		question:'What is the phase shift of $y = 3\\sin{\\left(2x-\\frac{\\pi}{2}\\right)}$?',
+		question:'Use identities to find the exact value of the trigonometric expression $$\dfrac{1}{\tan^2{\frac{\pi}{18}}} - \dfrac{1}{\sin^2{\frac{\pi}{18}}}.$$',
 		question_type: 'quiz',
-		answer_choices: ['$-\\frac{\\pi}{4}$',
-										 '$\\frac{\\pi}{2}$','$-\\pi$'],
-		correct: '$-\\frac{\\pi}{4}$',
-		module_name: 'sine'
+		answer_choices: ['$1$','$\\cot^2{\\frac{\\pi}{18}}$','$-1$'],
+		correct: '$-1$',
+		module_name: 'triangles'
+)
+Precalc.create(
+		question:'Does the following set of pairs define a function? $$\begin{array}{c|c} x & y \newline \hline 2 & 4
+ \newline -6 & -12 \newline 13 & 26 \newline -57 & -114 \end{array}$$',
+		question_type: 'quiz',
+		answer_choices: ['Yes','No','Only if you remove $(-57,-114)$'],
+		correct: 'Yes',
+		module_name: 'functions'
 )
