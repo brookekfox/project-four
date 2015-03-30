b64f951ca5310515276c5dee2e808b77de849eee
brookekfox <brookekfox@gmail.com>
2015-03-30 12:36:14 -0700

    linear added to dashboard

--- app/assets/javascripts/step_button.js
+++ app/assets/javascripts/step_button.js
@@ -1,18 +1,24 @@
 (function () {
 	var n = 2;
-	//  Make the current step be visible, and increment the step.
-	//  If it is the last step, disable the step button.
-	//  Once a step is taken, the reset button is made available.
 	window.ShowStep = function () {
-	document.getElementById("Step"+n).style.visibility = "visible"; n++;
-	if (!document.getElementById("Step"+n)) {document.getElementById("step").disabled = true}
-	document.getElementById("reset").disabled = false;
+		document.getElementById("Step"+n).style.visibility = "visible";
+		n++;
+		if (!document.getElementById("Step"+n)) {
+			document.getElementById("step").disabled = true;
+		}
+		document.getElementById("reset").disabled = false;
 	};
-	//  Enable the step button and disable the reset button.
 	window.ResetSteps = function () {
-	document.getElementById("step").disabled = false;
-	document.getElementById("reset").disabled = true;
-	var i = 1, step; n = 1;
-	while (step = document.getElementById("Step"+i)) {step.style.visibility = "hidden"; i++}
+		//if (document.getElementById("Step2") == null) {
+		//	document.getElementById("step").disabled = true;
+		//	document.getElementById("reset").disabled = false;
+		//}
+		document.getElementById("step").disabled = false;
+		document.getElementById("reset").disabled = true;
+		var i = 1, step; n = 1;
+		while (step = document.getElementById("Step"+i)) {
+			step.style.visibility = "hidden";
+			i++;
+		}
 	}
 })();
