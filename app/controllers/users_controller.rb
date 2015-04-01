70da750a2fa5547df4564427a4e3cd12ac5c5678
brookekfox <brookekfox@gmail.com>
2015-04-01 12:28:05 -0700

    precompiled assets locally

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 			session['user_id'] = @user.id.to_s
 			redirect_to user_path(session['user_id'])
 		else
-			redirect_to new_user_path
+			redirect_to root_path
 		end
 	end
 
